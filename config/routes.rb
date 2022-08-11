550f3687211c206d573b00162d3705469a48d0e6
Nate Tiller <tillerna@gmail.com>
2022-08-11 16:43:16 -0400

    Authentication updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   get 'welcome/gear'
   get 'welcome/services'
   get 'welcome/index'
+
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users, except: [:new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
